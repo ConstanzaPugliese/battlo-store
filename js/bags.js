ccff636e0345938ec0f56d291fe4c33c3d6fcb66
ConstanzaPugliese <conipugliese@gmail.com>
2021-08-27 00:43:00 -0300

    cambios carrito

--- js/bags.js
+++ js/bags.js
@@ -15,18 +15,21 @@ let currentCategory = `bags`
 // C A R R I T O //
 let shoppingCart = [];
 let total = 0;
-const DOMmodalBody = document.getElementById(`shopModalBody`);
-const DOMshoppingCart = document.getElementById(`shoppingCart`);
-const DOMbuttonEmpty = document.getElementById(`buttonEmpty`);
-const DOMtotal = document.getElementById(`total`);
-const DOMinstallmentValue = document.getElementById(`installmentValue`);
-const DOMbuttonBuy = document.getElementById(`buttonBuy`);
+const DOMtext = document.getElementById(`textBags`);
+const DOMshoppingCart = document.getElementById(`shoppingCartBags`);
+const DOMbuttonEmpty = document.getElementById(`buttonEmptyBags`);
+const DOMtotal = document.getElementById(`totalBags`);
+const DOMinstallmentValue = document.getElementById(`installmentValueBags`);
+const DOMbuttonBuy = document.getElementById(`buttonBuyBags`);
 const myLocalStorage = window.localStorage;
-const defaultText = document.createElement (`p`)
-defaultText.textContent = `Tu carrito de compras está vacío :(`;
-DOMmodalBody.appendChild(defaultText)
+DOMtext.textContent = `Tu carrito de compras está vacío :(`;
 
 /////////////////////////////////////////////// F U N C I O N E S ////////////////////////////////////////////////////
+
+function uploadProductsLocalStorage() {
+    const fromJson = localStorage.getItem(`products`);
+    const Products = JSON.parse(fromJson);
+}
 
 function filterCategory () {
     let category = Products.filter(Product => Product.category == currentCategory);
@@ -36,63 +39,63 @@ function printProducts() {
     filterCategory()
     let cards = document.getElementById(`cardsBags`)
     ProductsCategory.forEach (e => {
-        let div = document.createElement(`div`)
+        const div = document.createElement(`div`)
         div.setAttribute(`class`, `mb-4 col-lg-3 col-xs-12`)
         cards.appendChild(div)
-        let card = document.createElement(`div`)
+        const card = document.createElement(`div`)
         card.setAttribute(`class`, `card text-center`)
         div.appendChild(card)
-        let cardContainer = document.createElement(`div`)
+        const cardContainer = document.createElement(`div`)
         cardContainer.setAttribute(`class`, `card-container`)
         card.appendChild(cardContainer)
-        let imgLink = document.createElement(`a`)
+        const imgLink = document.createElement(`a`)
         imgLink.setAttribute(`href`, `${e.link}`)
         cardContainer.appendChild(imgLink)
-        let img = document.createElement(`img`)
+        const img = document.createElement(`img`)
         img.setAttribute(`src`, `${e.img}`)
         img.setAttribute(`class`, `card-img-top`)
         img.setAttribute(`alt`, `${e.name}`)
         imgLink.appendChild(img)
-        let jumbotron = document.createElement(`div`)
+        const jumbotron = document.createElement(`div`)
         jumbotron.setAttribute(`class`, `jumbotron jumbotron-absolute`)
         cardContainer.appendChild(jumbotron)
-        let spanJumbotron = document.createElement(`span`)
+        const spanJumbotron = document.createElement(`span`)
         spanJumbotron.setAttribute(`class`, `text-uppercase`)
         jumbotron.appendChild(spanJumbotron)
-        let boldSpanJumbotron = document.createElement(`b`)
+        const boldSpanJumbotron = document.createElement(`b`)
         boldSpanJumbotron.textContent = `${e.img_text}`
         spanJumbotron.prepend(boldSpanJumbotron)
-        let button = document.createElement(`button`)
+        const button = document.createElement(`button`)
         button.setAttribute(`type`, `button`)
         button.setAttribute(`class`, `btn btn-dark rounded-circle`)
         button.setAttribute(`marcador`, `${e.id}`)
         button.setAttribute(`data-target`, `#addModal`)
         cardContainer.appendChild(button)
-        let icon = document.createElement(`img`)
+        const icon = document.createElement(`img`)
         icon.setAttribute(`src`, `../assets/bag.svg`)
         icon.setAttribute(`class`, `bag-style`)
         button.appendChild(icon)
-        let cardBody = document.createElement(`div`)
+        const cardBody = document.createElement(`div`)
         cardBody.setAttribute(`class`, `card-body`)
         card.appendChild(cardBody)
-        let nameLink = document.createElement(`a`)
+        const nameLink = document.createElement(`a`)
         nameLink.setAttribute(`href`, `${e.link}`)
         cardBody.appendChild(nameLink)
-        let name = document.createElement(`h4`)
+        const name = document.createElement(`h4`)
         name.textContent = `${e.name}`
         nameLink.appendChild(name)
-        let price = document.createElement(`p`)
+        const price = document.createElement(`p`)
         cardBody.appendChild(price)
-        let boldPrice = document.createElement(`b`)
+        const boldPrice = document.createElement(`b`)
         boldPrice.textContent = `$${e.price}`
         price.appendChild(boldPrice)
-        let installment = document.createElement(`span`)
+        const installment = document.createElement(`span`)
         installment.textContent = `cuotas sin interés de `
         cardBody.appendChild(installment)
-        let installmentNumber = document.createElement(`b`)
+        const installmentNumber = document.createElement(`b`)
         installmentNumber.textContent = `6 `
         installment.prepend(installmentNumber)
-        let installmentValue = document.createElement(`b`)
+        const installmentValue = document.createElement(`b`)
         installmentValue.textContent = `$${e.installment_value}`
         installment.appendChild(installmentValue)
     })
@@ -107,9 +110,18 @@ function addProductShoppingCart(event) {
     printShoppingCart();
     saveShoppingCartLocalStorage();
 }
+function block () {
+    const div1 = document.getElementById(`noneBlockBags`)
+    div1.style.display = `block`;
+}
+function none () {
+    const div2 = document.getElementById(`noneBlockBags`)
+    div2.style.display = `none`;
+}
 function printShoppingCart() {
+    block();
+    DOMtext.textContent = `Éstos son los productos que elegiste:`;
     // Vacio todo el html
-    DOMmodalBody.textContent = ``;
     DOMshoppingCart.textContent = ``;
     // Quito los duplicados
     const shoppingCartNoDuplicate = [...new Set(shoppingCart)];
@@ -127,11 +139,11 @@ function printShoppingCart() {
         }, 0);
         // Nodo del item del carrito
         const myNode = document.createElement(`li`);
-        myNode.classList.add(`list-group-item`, `text-right`, `mx-2`);
+        myNode.setAttribute(`class`, `list-group-item mx-1`);
         myNode.textContent = `${amountItem} x ${myItem[0].name} - $${myItem[0].price}`;
         // Boton eliminar item
         const myButton = document.createElement(`button`);
-        myButton.setAttribute(`btn`, `border-0`, `mx-5`);
+        myButton.setAttribute(`class`, `btn border-0 mx-5`);
         myButton.dataset.item = item;
         myButton.addEventListener(`click`, deleteItemShoppingCart);
         myNode.appendChild(myButton);
@@ -141,54 +153,13 @@ function printShoppingCart() {
         myButton.appendChild(icon)
         DOMshoppingCart.appendChild(myNode);
     });
-    /*const div = document.createElement(`div`);
-    div.setAttribute(`class`, `text-right mt-2`);
-    DOMmodalBody.appendChild(div)
-    const buttonEmpty = document.createElement(`button`);
-    buttonEmpty.setAttribute(`class`, `btn btn-dark`);
-    buttonEmpty.textContent = `Vaciar`;
-    buttonEmpty.addEventListener(`click`, emptyShoppingCart);
-    div.appendChild(buttonEmpty);
-    const hr = document.createElement(`hr`);
-    DOMmodalBody.appendChild(hr);
-    const h3 = document.createElement(`h3`);
-    h3.setAttribute(`class`, `text-right`);
-    h3.textContent = `Total: $`;
-    DOMmodalBody.appendChild(h3);
-    const h3Price = document.createElement(`h3`);
-    h3Price.setAttribute(`id`, `total`);
-    h3.appendChild(h3Price);
-    const p = document.createElement(`p`);
-    p.setAttribute(`class`,`text-right`);
-    p.textContent = `0 a 6 cuotas sin interés de $`;
-    DOMmodalBody.appendChild(p);
-    const pPrice = document.createElement(`p`);
-    pPrice.setAttribute(`id`, `installment-value`);
-    p.appendChild(pPrice);
-    const div2 = document.createElement(`div`);
-    div2.setAttribute(`class`, `text-center mt-2`);
-    DOMmodalBody.appendChild(div2);
-    const buttonBuy = document.createElement(`button`);
-    buttonBuy.setAttribute(`class`, `btn btn-dark text-uppercase`);
-    buttonBuy.textContent = `Iniciar compra`;
-    buttonBuy.addEventListener(`click`, startPurchase);
-    div2.appendChild(buttonBuy);
-    const div3 = document.createElement(`div`)
-    div3.setAttribute(`class`, `text-center mt-2`);
-    DOMmodalBody.appendChild(div3)
-    const span = document.createElement(`span`);
-    div3.appendChild(span);
-    const link = document.createElement(`a`);
-    link.setAttribute(`href`, `../pages/products.html`);
-    link.textContent = `Ver más productos`;
-    span.appendChild(link);*/
 }
 function deleteItemShoppingCart(event) {
     // Obtenemos el producto ID que hay en el boton pulsado
-    const idItem = event.target.dataset.item;
+    const id = event.target.dataset.item;
     // Borramos todos los productos
     shoppingCart = shoppingCart.filter((shoppingCartId) => {
-        return shoppingCartId !== idItem;
+        return shoppingCartId !== id;
     });
     printShoppingCart();
     calculateTotal();
@@ -197,9 +168,9 @@ function deleteItemShoppingCart(event) {
 }
 function calculateTotal() {
     total = 0;
-    shoppingCart.forEach((item) => {
+    shoppingCart.forEach((shoppingCart) => {
         const myItem = ProductsCategory.filter((e) => {
-            return e.id === parseInt(item);
+            return e.id === parseInt(shoppingCart);
         });
         total = total + myItem[0].price;
     });
@@ -220,18 +191,18 @@ function emptyShoppingCart() {
     printShoppingCart();
     calculateTotal();
     calculateInstallment();
+    none();
+    DOMtext.textContent = `Tu carrito de compras está vacío :(`;
     localStorage.clear();
-    DOMmodalBody.appendChild(defaultText);
 }
 function startPurchase() {
     shoppingCart = [];
     printShoppingCart();
     calculateTotal();
     calculateInstallment();
+    none();
+    DOMtext.textContent = `Gracias por tu compra ;)`;
     localStorage.clear();
-    let text = document.createElement(`p`)
-    text.textContent = `Gracias por tu compra ;)`;
-    DOMmodalBody.appendChild(text)
 }
 function saveShoppingCartLocalStorage() {
     myLocalStorage.setItem(`shoppingCart`, JSON.stringify(shoppingCart))
@@ -268,6 +239,8 @@ function scrollTopButton(btn){
 // Eventos
 DOMbuttonEmpty.addEventListener(`click`, emptyShoppingCart);
 DOMbuttonBuy.addEventListener(`click`, startPurchase);
+// Cargo los productos del local storage
+uploadProductsLocalStorage();
 // Cargo los productos del carrito del local storage
 uploadShoppingCartLocalStorage();
 // Imprimo los productos
--- js/kylie.js
+++ js/kylie.js
@@ -15,19 +15,21 @@ let currentCategory = `Kylie`
 // C A R R I T O //
 let shoppingCart = [];
 let total = 0;
-const DOMmodalBody = document.getElementById(`shopModalBody`);
-const DOMshoppingCart = document.getElementById(`shoppingCart`);
-const DOMbuttonEmpty = document.getElementById(`buttonEmpty`);
-const DOMtotal = document.getElementById(`total`);
-const DOMinstallmentValue = document.getElementById(`installmentValue`);
-const DOMbuttonBuy = document.getElementById(`buttonBuy`);
+const DOMtext = document.getElementById(`textKylie`);
+const DOMshoppingCart = document.getElementById(`shoppingCartKylie`);
+const DOMbuttonEmpty = document.getElementById(`buttonEmptyKylie`);
+const DOMtotal = document.getElementById(`totalKylie`);
+const DOMinstallmentValue = document.getElementById(`installmentValueKylie`);
+const DOMbuttonBuy = document.getElementById(`buttonBuyKylie`);
 const myLocalStorage = window.localStorage;
-const defaultText = document.createElement (`p`)
-defaultText.textContent = `Tu carrito de compras está vacío :(`;
-DOMmodalBody.appendChild(defaultText)
+DOMtext.textContent = `Tu carrito de compras está vacío :(`;
 
 /////////////////////////////////////////////// F U N C I O N E S ////////////////////////////////////////////////////
 
+function uploadProductsLocalStorage() {
+    const fromJson = localStorage.getItem(`products`);
+    const Products = JSON.parse(fromJson);
+}
 function filterCategory () {
     let category = Products.filter(Product => Product.bag_name == currentCategory);
     ProductsCategory = category
@@ -107,9 +109,18 @@ function addProductShoppingCart(event) {
     printShoppingCart();
     saveShoppingCartLocalStorage();
 }
+function block () {
+    const div1 = document.getElementById(`noneBlockKylie`)
+    div1.style.display = `block`;
+}
+function none () {
+    const div2 = document.getElementById(`noneBlockKylie`)
+    div2.style.display = `none`;
+}
 function printShoppingCart() {
+    block();
+    DOMtext.textContent = `Éstos son los productos que elegiste:`;
     // Vacio todo el html
-    DOMmodalBody.textContent = ``;
     DOMshoppingCart.textContent = ``;
     // Quito los duplicados
     const shoppingCartNoDuplicate = [...new Set(shoppingCart)];
@@ -127,11 +138,11 @@ function printShoppingCart() {
         }, 0);
         // Nodo del item del carrito
         const myNode = document.createElement(`li`);
-        myNode.classList.add(`list-group-item`, `text-right`, `mx-2`);
+        myNode.setAttribute(`class`, `list-group-item mx-1`);
         myNode.textContent = `${amountItem} x ${myItem[0].name} - $${myItem[0].price}`;
         // Boton eliminar item
         const myButton = document.createElement(`button`);
-        myButton.setAttribute(`btn`, `border-0`, `mx-5`);
+        myButton.setAttribute(`class`, `btn border-0 mx-5`);
         myButton.dataset.item = item;
         myButton.addEventListener(`click`, deleteItemShoppingCart);
         myNode.appendChild(myButton);
@@ -141,54 +152,13 @@ function printShoppingCart() {
         myButton.appendChild(icon)
         DOMshoppingCart.appendChild(myNode);
     });
-    /*const div = document.createElement(`div`);
-    div.setAttribute(`class`, `text-right mt-2`);
-    DOMmodalBody.appendChild(div)
-    const buttonEmpty = document.createElement(`button`);
-    buttonEmpty.setAttribute(`class`, `btn btn-dark`);
-    buttonEmpty.textContent = `Vaciar`;
-    buttonEmpty.addEventListener(`click`, emptyShoppingCart);
-    div.appendChild(buttonEmpty);
-    const hr = document.createElement(`hr`);
-    DOMmodalBody.appendChild(hr);
-    const h3 = document.createElement(`h3`);
-    h3.setAttribute(`class`, `text-right`);
-    h3.textContent = `Total: $`;
-    DOMmodalBody.appendChild(h3);
-    const h3Price = document.createElement(`h3`);
-    h3Price.setAttribute(`id`, `total`);
-    h3.appendChild(h3Price);
-    const p = document.createElement(`p`);
-    p.setAttribute(`class`,`text-right`);
-    p.textContent = `0 a 6 cuotas sin interés de $`;
-    DOMmodalBody.appendChild(p);
-    const pPrice = document.createElement(`p`);
-    pPrice.setAttribute(`id`, `installment-value`);
-    p.appendChild(pPrice);
-    const div2 = document.createElement(`div`);
-    div2.setAttribute(`class`, `text-center mt-2`);
-    DOMmodalBody.appendChild(div2);
-    const buttonBuy = document.createElement(`button`);
-    buttonBuy.setAttribute(`class`, `btn btn-dark text-uppercase`);
-    buttonBuy.textContent = `Iniciar compra`;
-    buttonBuy.addEventListener(`click`, startPurchase);
-    div2.appendChild(buttonBuy);
-    const div3 = document.createElement(`div`)
-    div3.setAttribute(`class`, `text-center mt-2`);
-    DOMmodalBody.appendChild(div3)
-    const span = document.createElement(`span`);
-    div3.appendChild(span);
-    const link = document.createElement(`a`);
-    link.setAttribute(`href`, `../pages/products.html`);
-    link.textContent = `Ver más productos`;
-    span.appendChild(link);*/
 }
 function deleteItemShoppingCart(event) {
     // Obtenemos el producto ID que hay en el boton pulsado
-    const idItem = event.target.dataset.item;
+    const id = event.target.dataset.item;
     // Borramos todos los productos
     shoppingCart = shoppingCart.filter((shoppingCartId) => {
-        return shoppingCartId !== idItem;
+        return shoppingCartId !== id;
     });
     printShoppingCart();
     calculateTotal();
@@ -197,9 +167,9 @@ function deleteItemShoppingCart(event) {
 }
 function calculateTotal() {
     total = 0;
-    shoppingCart.forEach((item) => {
+    shoppingCart.forEach((shoppingCart) => {
         const myItem = ProductsCategory.filter((e) => {
-            return e.id === parseInt(item);
+            return e.id === parseInt(shoppingCart);
         });
         total = total + myItem[0].price;
     });
@@ -220,18 +190,18 @@ function emptyShoppingCart() {
     printShoppingCart();
     calculateTotal();
     calculateInstallment();
+    none();
+    DOMtext.textContent = `Tu carrito de compras está vacío :(`;
     localStorage.clear();
-    DOMmodalBody.appendChild(defaultText);
 }
 function startPurchase() {
     shoppingCart = [];
     printShoppingCart();
     calculateTotal();
     calculateInstallment();
+    none();
+    DOMtext.textContent = `Gracias por tu compra ;)`;
     localStorage.clear();
-    let text = document.createElement(`p`)
-    text.textContent = `Gracias por tu compra ;)`;
-    DOMmodalBody.appendChild(text)
 }
 function saveShoppingCartLocalStorage() {
     myLocalStorage.setItem(`shoppingCart`, JSON.stringify(shoppingCart))
@@ -248,6 +218,8 @@ function uploadShoppingCartLocalStorage() {
 // Eventos
 DOMbuttonEmpty.addEventListener(`click`, emptyShoppingCart);
 DOMbuttonBuy.addEventListener(`click`, startPurchase);
+// Cargo los productos del local storage
+uploadProductsLocalStorage();
 // Cargo los productos del carrito del local storage
 uploadShoppingCartLocalStorage();
 // Imprimo los productos
